c9302b068a86b7be622b74e63e93f435dd9556e7
Bullrich <javierbullrich@gmail.com>
2017-07-03 00:25:00 -0300

    Added namespace of Blue.Tree to BT

--- Assets/Blue/BehaviorTree/Scripts/Nodes/Node.cs
+++ Assets/Blue/BehaviorTree/Scripts/Nodes/Node.cs
@@ -1,23 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public abstract class Node {
+namespace Blue.Tree
+{
+    [System.Serializable]
+    public abstract class Node
+    {
+        ///<summary>Node posible states</summary>
+        public enum NodeStates
+        {
+            SUCCESS,
+            FAILURE,
+            RUNNING,
+        }
 
-    /* Delegate that returns the state of the node.*/
-    public delegate NodeStates NodeReturn();
+        ///<summary>Delegate that returns the state of the node.</summary>
+        public delegate NodeStates NodeReturn();
 
-    /* The current state of the node */
-    protected NodeStates m_nodeState;
+        ///<summary>The current state of the node</summary>
+        protected NodeStates m_nodeState;
 
-    public NodeStates nodeState {
-        get { return m_nodeState; }
+        public NodeStates nodeState
+        {
+            get { return m_nodeState; }
+        }
+
+        /* The constructor for the node */
+        public Node() { }
+
+        ///<summary>Implementing classes use this method to valuate the desired set of conditions</summary>
+        public abstract NodeStates Evaluate();
     }
-
-    /* The constructor for the node */
-    public Node() {}
-
-    /* Implementing classes use this method to valuate the desired set of conditions */
-    public abstract NodeStates Evaluate();
-
 }
